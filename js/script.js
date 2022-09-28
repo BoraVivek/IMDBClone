7c567b8b94d2e0372814d01c89c73bd6614506b0
Vivek Bora <vb8527@gmail.com>
2022-09-28 18:53:58 +0530

    Implement Search Result Add to DOM & Prevent Duplicate Adding

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 (function () {
+
     let movies = JSON.parse(window.localStorage.getItem('movies')) || [];
     let favouriteMovies = window.localStorage.getItem('favourite-movies') || [];
     const moviesList = document.querySelector("#movies-list");
@@ -21,7 +22,8 @@
 
     function addMovieToDOM(movie) {
         const article = document.createElement('article');
-        article.classList.add('movie', 'flex', 'relative', 'gap-2','pb-5', 'border-b-2');
+        article.classList.add('movie', 'flex', 'relative', 'gap-2', 'pb-5', 'border-b-2');
+        article.id = `movie-${movie.id}`;
         article.innerHTML = `
         <div class="image-poster relative h-52">
             <img src="${movie.poster}"
@@ -33,7 +35,7 @@
         </div>
         <div class="movie-info text-white flex flex-col justify-between w-full">
             <div class="movie-detail">
-                <div class="movie-title text-3xl" title="Movie Title">${movie.title}</div>
+                <div class="movie-title text-3xl hover:text-slate-800 cursor-pointer" title="Movie Title">${movie.title}</div>
                 <div class="movie-genre">${movie.genre}</div>
                 <div class="movie-plot" title="Movie Plot">
                     ${movie.plot}
@@ -62,49 +64,122 @@
 
     }
 
-    async function searchMovie(e){
+    async function searchMovie(e) {
         console.log("Searching Movie...", e.target.value);
         searchResults = [];
         searchResultContainer.classList.add('hidden');
-        
+
         let searchText = e.target.value;
-        const response = await fetch(`${API_URL}/?apikey=${API_KEY}&s=${searchText}&type=movie`);
+        const response = await fetch(`${API_URL}/?apikey=${API_KEY}&s=${searchText}`);
         const data = await response.json();
 
-        if(data.Response != 'False'){
+        if (data.Response != 'False') {
             searchResults.push(...data.Search);
         }
 
-        if(searchResults.length > 0){
+        if (searchResults.length > 0) {
             searchResultContainer.classList.remove('hidden');
         }
 
         console.log("Search Data", data);
         console.log("Search Results", searchResults);
 
+        searchResults.forEach((movie) => {
+            addSearchResultToDOM(movie);
+        })
+    }
+
+
+
+    /** Function to Add Search Result to DOM */
+    function addSearchResultToDOM(movie) {
+        const div = document.createElement('div');
+        div.classList.add("search-item", "flex", "p-2", 'gap-3', 'text-white', 'border-b-2', 'items-center', 'justify-between');
+        div.innerHTML = `
+            <div class="flex gap-3">
+                <img src="${movie.Poster ? movie.Poster : './img/no-poster-available.jpg'}" 
+                    alt="${movie.Title}" class="h-24 w-20 rounded">
+                <div class="movie-info">
+                    <div class="movie-title text-lg" title="Movie Title">${movie.Title}</div>
+                    <div class="movie-year text-sm text-gray-400" title="Release Year">${movie.Year}</div>
+                </div>
+            </div>
+            <button class='addMovieToList' data-movieId=${movie.imdbID}>
+                <i class="fas fa-plus-circle text-2xl pointer-events-none" title="Add Movie to List"></i>
+            </button>
+        `;
+
+        searchResultContainer.append(div);
+    }
+
+    function test() {
+        alert("Hello");
+    }
+
+    /** Function to add Movie to the list from the search results */
+    async function addMovieToList(movieId) {
+        console.log("Movie ID:", movieId);
+        if (!movieId) {
+            return;
+        }
+
+        let movieExists = movies.find((movie) => movie.id == movieId);
+
+        if(movieExists){
+            alert("Movie Already Exists!");
+            return;
+        }
+
+        let response = await fetch(`${API_URL}/?apikey=${API_KEY}&i=${movieId}`);
+        let data = await response.json();
+
+        if (data.Response == 'False') {
+            return;
+        }
+
+        //Creating new Movie Object
+        let newMovie = {
+            id: data.imdbID,
+            title: data.Title,
+            year: data.Year,
+            runTime: data.Runtime,
+            genre: data.Genre,
+            plot: data.Plot,
+            poster: data.Poster,
+            imdbRating: data.imdbRating,
+            rottenTomatoes: data.Ratings.find((rating) => rating.Source == 'Rotten Tomatoes')?.Value || "N/A"
+        }
+
+        //Adding Movie to the Movies Array, and in the Localstorge
+        addMovie(newMovie);
+
+        //Adding movie to the DOM
+        addMovieToDOM(newMovie);
     }
 
     function removeFromFavourite(movie) {
 
     }
 
+    /** Function to load initial movies */
     function loadInitialMovies() {
         console.log("Loading Movies...");
-        console.log("Movies",movies);
+        console.log("Movies", movies);
 
+        // Check if initial movies exist
         if (movies.length == 0) {
+
+            //If initial movies don't exist, than we load some default movies in UI
             const INITIAL_MOVIES = [
                 'tt6277462', 'tt8178634', 'tt10028196'
             ];
 
-            console.log(INITIAL_MOVIES);
-
+            //Looping through all Movie IDs, and fetching their Data
             INITIAL_MOVIES.forEach(async (movie) => {
-                let response = await fetch(`${API_URL}/?apikey=${API_KEY}&i=${movie}&type=movie&plot=full`);
+                let response = await fetch(`${API_URL}/?apikey=${API_KEY}&i=${movie}&plot=full`);
                 let data = await response.json();
 
-                console.log(data);
-
+                //Creating new Movie Object
                 let newMovie = {
                     id: data.imdbID,
                     title: data.Title,
@@ -117,22 +192,41 @@
                     rottenTomatoes: data.Ratings.find((rating) => rating.Source == 'Rotten Tomatoes').Value
                 }
 
-                console.log(newMovie);
-
+                //Adding Movie to the Movies Array, and in the Localstorge
                 addMovie(newMovie);
+
+                //Adding movie to the DOM
                 addMovieToDOM(newMovie);
             })
         } else {
+            /** If movies exists in localstorage, than we render them in the DOM */
             movies.forEach((movie) => {
                 addMovieToDOM(movie);
             })
         }
     }
 
+    function handleClickEvent(e) {
+        let target = e.target;
+
+        switch (target.className) {
+            case "addMovieToList":
+                let movieId = target.dataset.movieid;
+                addMovieToList(movieId);
+                break;
+            default:
+                console.log("No Event");
+                break;
+        }
+    }
+
+    /** Initialize Application */
     function initializeApp() {
         loadInitialMovies();
-        searchBox.addEventListener("change", searchMovie);
+        searchBox.addEventListener("change", searchMovie); //Adding onChange event to searchBox for handling search 
+        document.addEventListener("click", handleClickEvent);
     }
 
     initializeApp();
-})()
+
+})();
